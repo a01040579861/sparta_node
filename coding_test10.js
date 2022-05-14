d650adb818de9ee9be6372f0e977edf379c31b15
seongIllee <a01040579861@gmail.com>
2022-05-14 10:18:38 +0900

    걷기반 문제 마라톤 10번 문제 풀이

--- coding_test10.js
+++ coding_test10.js
@@ -1,6 +1,5 @@
 function solution(arr1, arr2) {
-//var answer = [[]]; //맨처음 문제 조건은 이렇게 되있었는데 방법을 모르는듯 하여 제가 바꿨습니다.
-    var answer = []; //answer 초기값을 배열로 할당
+    var answer = [[]]; //answer 초기값을 배열로 할당
     for(let i=0; i<arr1.length; i++) { //첫번째 배열의 크기만큼 반복문을 돌린다.
         answer[i] = []; //answer 에 i번째 행을 추가
             for(let j=0; j<arr1[i].length; j++) { //행의 갯수만큼 반복문을 돌린다.
